7d49173748048016ec830f718b4c35e4bbf2b161
anizhatyas123 <62780444+anizhatyas123@users.noreply.github.com>
2020-04-27 12:22:07 +0700

    Add files via upload

---
+++ Perkalian.java
@@ -0,0 +1,24 @@
+package perkalian;
+
+import java.util.Scanner;
+public class Perkalian {
+    void perkalian(){
+        int perkalian,x,y;
+        Scanner masukan = new Scanner(System.in);
+        System.out.print("Tabel Perkalian :n");
+        perkalian = masukan.nextInt();
+        System.out.print("x ");
+        for(x=0;x<=perkalian;x++){
+            System.out.print(x +"");
+        }
+        System.out.println("");
+        for(x=0;x<=perkalian;x++){
+            System.out.print(x +" ");
+        for(y=0;y<=perkalian;y++){
+            System.out.print(y*x +" ");
+        }
+        System.out.println("");
+        }
+    }
+    
+}
