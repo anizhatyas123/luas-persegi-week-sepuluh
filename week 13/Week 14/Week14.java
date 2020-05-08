a24010e40fc9f68125bd5a25392de0ba1b14564e
anizhatyas123 <62780444+anizhatyas123@users.noreply.github.com>
2020-05-07 18:48:28 -0700

    Add files via upload

---
+++ week 13/Week 14/Week14.java
@@ -0,0 +1,35 @@
+package week.pkg14;
+
+import java.util.Scanner;
+
+public class Week14 {
+    void ratarata(){
+        int array[], i, n;
+        array = new int[100];
+        float rata, total=0;
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukkan banyaknya elemen array: ");
+        n = scan.nextInt();
+        
+        for(i=1; i<=n; i++){
+            System.out.print("Nilai ke-"+i+":");
+            array[i]=scan.nextInt();
+            total=total+array[i];
+        }
+        
+        if (n>50){
+            System.out.print("data besar");
+        }
+        else {
+            System.out.print("data kecil");
+        }
+        
+        rata=total/n;
+        System.out.println("Hasil nilai total adalah:"+total);
+        System.out.println("Hasil rata-rata adalah:"+rata);
+     
+        } 
+    }
+    
+
